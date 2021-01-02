9336d494a49bd86259c8ad41676d68a019b9f3ee
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-01-02 16:16:26 +0300

    lint fix

--- src/services/authenticationService.ts
+++ src/services/authenticationService.ts
@@ -1,21 +1,21 @@
 import { ClientConfig } from '../clientConfig';
 
+function getBasicAuthenticationToken(
+  basicAuthentication: ClientConfig.Authentication.Basic,
+): string {
+  const {
+    username,
+    password,
+    apiToken,
+  } = basicAuthentication;
+
+  const buffer = Buffer.from(`${username}:${apiToken ?? password}`);
+  const token = buffer.toString('base64');
+
+  return `Basic ${token}`;
+}
+
 export namespace AuthenticationService {
-  function getBasicAuthenticationToken(
-    basicAuthentication: ClientConfig.Authentication.Basic,
-  ): string {
-    const {
-      username,
-      password,
-      apiToken,
-    } = basicAuthentication;
-
-    const buffer = Buffer.from(`${username}:${apiToken ?? password}`);
-    const token = buffer.toString('base64');
-
-    return `Basic ${token}`;
-  }
-
   export function getAuthenticationToken(authentication?: ClientConfig.Authentication): string | undefined {
     if (!authentication) {
       return undefined;
