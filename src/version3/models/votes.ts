65dfdb279413e3ab8e78ee4004403ada8873387f
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-07-08 20:01:50 +0300

    #144 fixed error when tried vote issue

--- src/version3/models/votes.ts
+++ src/version3/models/votes.ts
@@ -3,14 +3,14 @@ import { User } from './user';
 /** The details of votes on an issue. */
 export interface Votes {
   /** The URL of these issue vote details. */
-  self?: string;
+  self: string;
   /** The number of votes on the issue. */
-  votes?: number;
+  votes: number;
   /** Whether the user making this request has voted on the issue. */
-  hasVoted?: boolean;
+  hasVoted: boolean;
   /**
    * List of the users who have voted on this issue. An empty list is returned when the calling user doesn't have the
    * *View voters and watchers* project permission.
    */
-  voters?: User[];
+  voters: User[];
 }
