5ceba35fc149030321f16c51f1658b0ff23e8cdc
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-08-30 18:08:53 +0300

    build fixed

--- src/version2/parameters/createDashboard.ts
+++ src/version2/parameters/createDashboard.ts
@@ -1,3 +1,6 @@
-import { DashboardDetails } from '../models';
+import { DashboardDetails, SharePermission } from '../models';
 
-export interface CreateDashboard extends Omit<DashboardDetails, 'editPermissions'> {}
+export interface CreateDashboard extends Omit<DashboardDetails, 'editPermissions'> {
+  /** The edit permissions for the dashboard. */
+  editPermissions?: SharePermission[];
+}
