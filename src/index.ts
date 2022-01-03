16643217e629cf04edbb70bbc5f1a2d0114221f0
Vladislav Tupikin <mrrefactoring@yandex.ru>
2022-01-04 00:26:56 +0300

    service desk added to index

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export * from './paginated';
 export * as Agile from './agile';
 export * as Version2 from './version2';
 export * as Version3 from './version3';
+export * as ServiceDesk from './serviceDesk';
