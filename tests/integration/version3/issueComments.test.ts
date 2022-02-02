4f8039532bf6c449a9659074ed2f9aa4aec942a9
Rafael Dohms <rdohms@gmail.com>
2022-02-02 21:46:48 +0100

    Fix updateComment payload
    Payload is currently set to send the body as a string, this causes
    payload to be encoded as form-data not json. Passing a json object
    solves this and avoids the 405 error from the API.

---
+++ tests/integration/version3/issueComments.test.ts
@@ -0,0 +1,54 @@
+import {
+  cleanupEnvironment,
+  getVersion3Client,
+  prepareEnvironment,
+} from '../utils';
+import { Constants } from '../constants';
+
+describe('IssueAttachments', () => {
+  beforeAll(async () => {
+    await prepareEnvironment();
+  });
+
+  afterAll(async () => {
+    await cleanupEnvironment();
+  });
+
+  it('should update comment', async () => {
+    const client = getVersion3Client({ noCheckAtlassianToken: true });
+
+    const issue = await client.issues.createIssue({
+      fields: {
+        summary: 'Issue with comments',
+        project: {
+          key: Constants.testProjectKey,
+        },
+        issuetype: {
+          name: 'Task',
+        },
+      },
+    });
+
+    expect(issue).toBeDefined();
+
+    const comment = await client.issueComments.addComment({
+      issueIdOrKey: issue.key,
+      body: 'this is a comment',
+    });
+
+    expect(comment).toBeDefined();
+
+    const updatedComment = await client.issueComments.updateComment({
+      issueIdOrKey: issue.key,
+      id: comment.id,
+      body: 'updated comment',
+    });
+
+    expect(updatedComment).toBeDefined();
+    expect(updatedComment.id).toEqual(comment.id);
+
+    await client.issues.deleteIssue({
+      issueIdOrKey: issue.key,
+    });
+  });
+});
