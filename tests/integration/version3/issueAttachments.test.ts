1d450c38acf6f282971fa73821a50f21aff31a34
Vladislav Tupikin <MrRefactoring@yandex.ru>
2022-07-25 00:44:13 +0400

    #221 responseType fixed for getAttachmentContent (#222)

--- tests/integration/version3/issueAttachments.test.ts
+++ tests/integration/version3/issueAttachments.test.ts
@@ -1,7 +1,13 @@
 import * as fs from 'fs';
 import { Constants } from '../constants';
 import test from 'ava';
+import { Attachment, Issue } from '../../../src/version3/models';
 import { cleanupEnvironment, getVersion3Client, prepareEnvironment } from '../utils';
+
+const client = getVersion3Client({ noCheckAtlassianToken: true });
+
+let issue: Issue;
+let attachments: Attachment[];
 
 test.before(async () => {
   await prepareEnvironment();
@@ -12,9 +18,7 @@ test.after(async () => {
 });
 
 test.serial('should add attachment', async t => {
-  const client = getVersion3Client({ noCheckAtlassianToken: true });
-
-  const issue = await client.issues.createIssue({
+  issue = await client.issues.createIssue({
     fields: {
       summary: 'Issue with attachment',
       project: {
@@ -28,19 +32,29 @@ test.serial('should add attachment', async t => {
 
   t.truthy(!!issue);
 
-  const attachments = await client.issueAttachments.addAttachment({
+  attachments = await client.issueAttachments.addAttachment({
     issueIdOrKey: issue.key,
     attachment: {
       filename: 'issueAttachments.test.ts',
-      file: fs.readFileSync('./tests/integration/version2/issueAttachments.test.ts'),
+      file: fs.readFileSync('./tests/integration/version3/issueAttachments.test.ts'),
     },
   });
 
   t.truthy(!!attachments);
   t.is(attachments[0].filename, 'issueAttachments.test.ts');
   t.is(attachments[0].mimeType, 'video/mp2t');
+});
 
+test.serial('should getAttachmentContent', async t => {
+  const content = await client.issueAttachments.getAttachmentContent({ id: attachments[0].id });
+
+  t.truthy(Buffer.isBuffer(content));
+});
+
+test.serial('should remove attachment', async t => {
   await client.issues.deleteIssue({
     issueIdOrKey: issue.key,
   });
+
+  t.pass();
 });
