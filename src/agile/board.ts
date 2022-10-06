5ed642392642fecc6b71b9895e9f6e154a712628
Vladislav Tupikin <mrrefactoring@yandex.ru>
2022-10-06 22:13:10 +0400

    dependencies updated

--- src/agile/board.ts
+++ src/agile/board.ts
@@ -50,9 +50,7 @@ export class Board {
    * <code>location</code> must include the <code>type</code> property (Valid values: project, user). If choosing
    * 'project', then a project must be specified by a <code>projectKeyOrId</code> property in <code>location</code>. If
    * choosing 'user', the current user is chosen by default. The <code>projectKeyOrId</code> property should not be
-   * provided. </li> </ul> <p>
-   *  Note:
-   *  <ul>
+   * provided. </li> </ul> <p> Note: <ul>
    *  <li>
    *  If you want to create a new project with an associated board, use the <a href="https://docs.atlassian.com/jira/REST/latest">Jira platform REST API</a>.
    *  For more information, see the <a href="#api-rest-api-<ver>-project-post">Create project</a> method.
@@ -78,9 +76,7 @@ export class Board {
    * <code>location</code> must include the <code>type</code> property (Valid values: project, user). If choosing
    * 'project', then a project must be specified by a <code>projectKeyOrId</code> property in <code>location</code>. If
    * choosing 'user', the current user is chosen by default. The <code>projectKeyOrId</code> property should not be
-   * provided. </li> </ul> <p>
-   *  Note:
-   *  <ul>
+   * provided. </li> </ul> <p> Note: <ul>
    *  <li>
    *  If you want to create a new project with an associated board, use the <a href="https://docs.atlassian.com/jira/REST/latest">Jira platform REST API</a>.
    *  For more information, see the <a href="#api-rest-api-<ver>-project-post">Create project</a> method.
@@ -228,12 +224,11 @@ export class Board {
    * board.</li> <li><code>name</code> - Name of the board.</li> <li><code>filter</code> - Reference to the filter used
    * by the given board.</li> <li><code>location</code> - Reference to the container that the board is located in.
    * Includes the container type (Valid values: project, user). </li> <li><code>subQuery</code> (Kanban only) - JQL
-   * subquery used by the given board.</li> <li><code>columnConfig</code> - The column configuration lists the columns for the board, in the order defined in the column configuration.
-   *  For each column, it shows the issue status mapping
-   *  as well as the constraint type (Valid values: none, issueCount, issueCountExclSubs) for the min/max number of issues.
-   *  Note, the last column with statuses mapped to it is treated as the "Done" column,
-   *  which means that issues in that column will be marked as already completed.</li>
-   *  <li><code>estimation</code> (Scrum only) - Contains information about type of estimation used for the board. Valid values: none, issueCount, field.
+   * subquery used by the given board.</li> <li><code>columnConfig</code> - The column configuration lists the columns
+   * for the board, in the order defined in the column configuration. For each column, it shows the issue status mapping
+   * as well as the constraint type (Valid values: none, issueCount, issueCountExclSubs) for the min/max number of
+   * issues. Note, the last column with statuses mapped to it is treated as the "Done" column, which means that issues
+   * in that column will be marked as already completed.</li> <li><code>estimation</code> (Scrum only) - Contains information about type of estimation used for the board. Valid values: none, issueCount, field.
    *  If the estimation type is "field", the ID and display name of the field used for estimation is also returned.
    *  Note, estimates for an issue can be updated by a PUT /rest/api/~ver~/issue/{issueIdOrKey} request, however the fields must be on the screen.
    *  "timeoriginalestimate" field will never be on the screen, so in order to update it "originalEstimate" in "timetracking" field should be updated.
@@ -250,12 +245,11 @@ export class Board {
    * board.</li> <li><code>name</code> - Name of the board.</li> <li><code>filter</code> - Reference to the filter used
    * by the given board.</li> <li><code>location</code> - Reference to the container that the board is located in.
    * Includes the container type (Valid values: project, user). </li> <li><code>subQuery</code> (Kanban only) - JQL
-   * subquery used by the given board.</li> <li><code>columnConfig</code> - The column configuration lists the columns for the board, in the order defined in the column configuration.
-   *  For each column, it shows the issue status mapping
-   *  as well as the constraint type (Valid values: none, issueCount, issueCountExclSubs) for the min/max number of issues.
-   *  Note, the last column with statuses mapped to it is treated as the "Done" column,
-   *  which means that issues in that column will be marked as already completed.</li>
-   *  <li><code>estimation</code> (Scrum only) - Contains information about type of estimation used for the board. Valid values: none, issueCount, field.
+   * subquery used by the given board.</li> <li><code>columnConfig</code> - The column configuration lists the columns
+   * for the board, in the order defined in the column configuration. For each column, it shows the issue status mapping
+   * as well as the constraint type (Valid values: none, issueCount, issueCountExclSubs) for the min/max number of
+   * issues. Note, the last column with statuses mapped to it is treated as the "Done" column, which means that issues
+   * in that column will be marked as already completed.</li> <li><code>estimation</code> (Scrum only) - Contains information about type of estimation used for the board. Valid values: none, issueCount, field.
    *  If the estimation type is "field", the ID and display name of the field used for estimation is also returned.
    *  Note, estimates for an issue can be updated by a PUT /rest/api/~ver~/issue/{issueIdOrKey} request, however the fields must be on the screen.
    *  "timeoriginalestimate" field will never be on the screen, so in order to update it "originalEstimate" in "timetracking" field should be updated.
