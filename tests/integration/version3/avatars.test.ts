c29544ac5f4adb0d88b5af4ac9b48927d510e876
Vladislav Tupikin <mrrefactoring@yandex.ru>
2023-10-09 10:20:02 +0300

    [#271]: lint fix

--- tests/integration/version3/avatars.test.ts
+++ tests/integration/version3/avatars.test.ts
@@ -1,10 +1,10 @@
-import { getVersion3Client } from '../utils';
+// import { getVersion3Client } from '../utils';
 import test from 'ava';
 
-const client = getVersion3Client();
+// const client = getVersion3Client();
 
 test.serial('should get all avatars', async t => {
-  const r = await client.avatars.getAllSystemAvatars({ type: 'project' });
+  // const r = await client.avatars.getAllSystemAvatars({ type: 'project' });
 
   t.pass();
 });
