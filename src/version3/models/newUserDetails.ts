362918093c20036049db334743e2a0f5f41cbcd4
Appelberg-s <Appelberg-s@users.noreply.github.com>
2024-12-29 19:30:05 +0100

    Fixed create user parameters for v3 (#331)
    * Fixed create user parameters for v3
    
    * Update src/version3/models/newUserDetails.ts
    
    Co-authored-by: Vladislav Tupikin <MrRefactoring@yandex.ru>
    
    ---------
    
    Co-authored-by: Vladislav Tupikin <MrRefactoring@yandex.ru>

--- src/version3/models/newUserDetails.ts
+++ src/version3/models/newUserDetails.ts
@@ -4,4 +4,10 @@ export interface NewUserDetails {
   self?: string;
   /** The email address for the user. */
   emailAddress: string;
+  /**
+   * Products the new user has access to. Valid products are: jira-core, jira-servicedesk, jira-product-discovery,
+   * jira-software. To create a user without product access, set this field to be an empty array.
+   * Defaults to ['jira-core', 'jira-servicedesk', 'jira-product-discovery', 'jira-software'].
+   */
+  products?: ('jira-core' | 'jira-servicedesk' | 'jira-product-discovery' | 'jira-software' | string)[];
 }
