38ebced9b14f66f7214f918a6d5d0dbf431a689b
Ness Li <nessli2002@gmail.com>
2025-02-06 03:20:14 +0000

    test: define unit test for v2 jiraExpressions
    Only implemented unit test for the newly added function evaluateJiraExpressionUsingEnhancedSearch()

---
+++ tests/unit/version2/jiraExpressions.test.ts
@@ -0,0 +1,40 @@
+import * as sinon from 'sinon';
+import { test } from 'vitest';
+import { JiraExpressions, Version2Client } from '@jirajs/version2';
+
+const config = { host: 'http://localhost' };
+
+test('should be defined', ({ expect }) => {
+  expect(!!JiraExpressions).toBeTruthy();
+});
+
+test('evaluateJiraExpressionUsingEnhancedSearch should calls without parameters', ({ expect }) => {
+  const client = new Version2Client(config);
+  const sendRequestStub = sinon.stub(client, 'sendRequest');
+
+  client.jiraExpressions.evaluateJiraExpressionUsingEnhancedSearch();
+
+  expect(sendRequestStub.calledOnce).toBeTruthy();
+});
+
+test('evaluateJiraExpressionUsingEnhancedSearch should accept follow parameters', ({ expect }) => {
+  const client = new Version2Client(config);
+  const sendRequestStub = sinon.stub(client, 'sendRequest');
+
+  client.jiraExpressions.evaluateJiraExpressionUsingEnhancedSearch({
+    expression: '{ key: issue.key, type: issue.issueType.name }',
+  });
+
+  expect(sendRequestStub.calledOnce).toBeTruthy();
+
+  const callArgument = sendRequestStub.getCall(0).args[0];
+
+  expect(callArgument.params).toStrictEqual({
+    expand: undefined,
+  });
+
+  expect(callArgument.data).toStrictEqual({
+    expression: '{ key: issue.key, type: issue.issueType.name }',
+    context: undefined,
+  });
+});
