be207d8b8a37fc5163348af1c234d8f3e764fbfd
Ness Li <nessli2002@gmail.com>
2025-02-09 13:24:11 +0000

    fix: duplicate key

--- src/version3/parameters/createProjectAvatar.ts
+++ src/version3/parameters/createProjectAvatar.ts
@@ -1,7 +1,6 @@
 export interface CreateProjectAvatar {
   /** The ID or (case-sensitive) key of the project. */
   projectIdOrKey: string | number;
-  avatar: any;
   /** The X coordinate of the top-left corner of the crop region. */
   x?: number;
   /** The Y coordinate of the top-left corner of the crop region. */
