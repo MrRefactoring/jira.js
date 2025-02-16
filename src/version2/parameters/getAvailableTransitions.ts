cdd541a41f18d4368e273c4ef7378496b12f5b4f
Ness Li <nessgor@users.noreply.github.com>
2025-02-16 01:05:22 +0000

    Update src/version2/parameters/getAvailableTransitions.ts
    add number support to issue ids or keys
    
    Co-authored-by: Vladislav Tupikin <MrRefactoring@yandex.ru>

--- src/version2/parameters/getAvailableTransitions.ts
+++ src/version2/parameters/getAvailableTransitions.ts
@@ -1,6 +1,6 @@
 export interface GetAvailableTransitions {
   /** Ids or keys of the issues to get transitions available for them. */
-  issueIdsOrKeys: string[];
+  issueIdsOrKeys: (number | string)[];
   /** The end cursor for use in pagination. */
   endingBefore?: string;
   /** The start cursor for use in pagination. */
