d95dade1c7d4a0128c181608302d6c83e6cad5bd
Ness Li <nessgor@users.noreply.github.com>
2025-02-16 01:06:12 +0000

    Update src/version2/parameters/getBulkEditableFields.ts
    add number support to issueIdsOrKeys
    
    Co-authored-by: Vladislav Tupikin <MrRefactoring@yandex.ru>

--- src/version2/parameters/getBulkEditableFields.ts
+++ src/version2/parameters/getBulkEditableFields.ts
@@ -1,6 +1,6 @@
 export interface GetBulkEditableFields {
   /** The IDs or keys of the issues to get editable fields from. */
-  issueIdsOrKeys: string;
+  issueIdsOrKeys: number | string;
   /** (Optional)The text to search for in the editable fields. */
   searchText?: string;
   /** (Optional)The end cursor for use in pagination. */
