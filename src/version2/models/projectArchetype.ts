6389165f946efaff954e0fed9dd9ea6c4de0ed2c
Vladislav Tupikin <mrrefactoring@yandex.ru>
2026-01-25 23:01:44 +0300

    lint fix

--- src/version2/models/projectArchetype.ts
+++ src/version2/models/projectArchetype.ts
@@ -1,5 +1,5 @@
 export interface ProjectArchetype {
-    realType?: "BUSINESS" | "SOFTWARE" | "PRODUCT_DISCOVERY" | "SERVICE_DESK" | "CUSTOMER_SERVICE" | "OPS" | string;
-    style?: "classic" | "next-gen" | string;
-    type?: "BUSINESS" | "SOFTWARE" | "PRODUCT_DISCOVERY" | "SERVICE_DESK" | "CUSTOMER_SERVICE" | "OPS" | string;
+  realType?: 'BUSINESS' | 'SOFTWARE' | 'PRODUCT_DISCOVERY' | 'SERVICE_DESK' | 'CUSTOMER_SERVICE' | 'OPS' | string;
+  style?: 'classic' | 'next-gen' | string;
+  type?: 'BUSINESS' | 'SOFTWARE' | 'PRODUCT_DISCOVERY' | 'SERVICE_DESK' | 'CUSTOMER_SERVICE' | 'OPS' | string;
 }
