82b0f6ee885fdd0d5677bb4a2ec7f058610efd15
Vladislav Tupikin <mrrefactoring@yandex.ru>
2026-01-26 10:53:42 +0300

    #411 update webhook model

--- src/version2/models/webhook.ts
+++ src/version2/models/webhook.ts
@@ -1,7 +1,17 @@
 /** A webhook. */
 export interface Webhook {
   /** The Jira events that trigger the webhook. */
-  events: string[];
+  events: (
+    | 'jira:issue_created'
+    | 'jira:issue_updated'
+    | 'jira:issue_deleted'
+    | 'comment_created'
+    | 'comment_updated'
+    | 'comment_deleted'
+    | 'issue_property_set'
+    | 'issue_property_deleted'
+    | string
+  )[];
   /**
    * The date after which the webhook is no longer sent. Use [Extend webhook
    * life](https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-webhooks/#api-rest-api-2-webhook-refresh-put)
@@ -23,4 +33,6 @@ export interface Webhook {
   issuePropertyKeysFilter?: string[];
   /** The JQL filter that specifies which issues the webhook is sent for. */
   jqlFilter: string;
+  /** The URL that specifies where the webhooks are sent. */
+  url: string;
 }
